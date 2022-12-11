51eaeb6a7b48d30b9a57a4067ffccf337aa39a7b
zonekrish <107716360+zonekrish@users.noreply.github.com>
2022-12-10 18:30:15 -0600

    Add proper source code

--- main.py
+++ main.py
@@ -1,49 +1,64 @@
+# Main dependencies
 import tweepy
-import datetime
-from lyricsgenius import Genius
-from secret import credentials, log
+import lyricsgenius
+
+# Dependencies to run every hour
+import schedule
+import time
+import os
+from server import keep_alive
 
 client = tweepy.Client(
-    credentials['BEARER_TOKEN'],
-    credentials['API_KEY'],
-    credentials['API_SECRET'],
-    credentials['ACCESS_TOKEN'],
-    credentials['ACCESS_SECRET']
+    os.environ['BEARER_TOKEN'],
+    os.environ['API_KEY'],
+    os.environ['API_SECRET'],
+    os.environ['ACCESS_TOKEN'],
+    os.environ['ACCESS_SECRET']
 )
 
 auth = tweepy.OAuth1UserHandler(
-    credentials['API_KEY'],
-    credentials['API_SECRET'],
-    credentials['ACCESS_TOKEN'],
-    credentials['ACCESS_SECRET']
+    os.environ['API_KEY'],
+    os.environ['API_SECRET'],
+    os.environ['ACCESS_TOKEN'],
+    os.environ['ACCESS_SECRET']
 )
 api = tweepy.API(auth)
 
-genius = Genius(credentials['GENIUS_TOKEN'])
+genius = lyricsgenius.API(os.environ['GENIUS_TOKEN'])
 
-lyrics = api.user_timeline(user_id=1264110032875999234, count=1)
-    
-for tweet in lyrics:
-    lyric = tweet.text.strip()
-    tweetID = tweet.id
+def reply():
+  lyrics = api.user_timeline(user_id=1264110032875999234, count=1)
+      
+  for tweet in lyrics:
+      lyric = tweet.text.strip()
+      tweetID = tweet.id
+  
+  song = genius.search_songs(lyric)['hits'][0]['result']
+  id = song['id']
+  songData = genius.song(id)
+  
+  songName = genius.search_songs(lyric)['hits'][0]['result']['title']
+  
+  if(songData['song']['album'] == None):
+      albumName = songName
+  else:
+      albumName = songData['song']['album']['name'].strip()
+  
+  if(albumName == 'FO3*'):
+      albumName = songName
+  
+  if(albumName.endswith('.')):
+      reply = f'"{songName}" from album {albumName}'
+  else:
+      reply = f'"{songName}" from album {albumName}.'
+  
+  client.create_tweet(text=reply, in_reply_to_tweet_id=tweetID)
+  print(reply)
 
-song = genius.search_lyrics(lyric)
-id = song['sections'][0]['hits'][0]['result']['id']
-songData = genius.song(id)
+keep_alive()
 
-songName = song['sections'][0]['hits'][0]['result']['title']
-if(songData['song']['album'] == None):
-    albumName = songName
-else:
-    albumName = songData['song']['album']['name'].strip()
+schedule.every().hour.at(':16').do(reply)
 
-if(albumName == 'FO3*'):
-    albumName = songName
-
-if(albumName.endswith('.')):
-    reply = f'"{songName}" from album {albumName}'
-else:
-    reply = f'"{songName}" from album {albumName}.'
-
-client.create_tweet(text=reply, in_reply_to_tweet_id=tweetID)
-log(datetime.datetime.now())
+while True:
+  schedule.run_pending()
+  time.sleep(1)
