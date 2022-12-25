39fad5af97d49eaf8e310108d739db5ceee5fb4e
zonekrish <107716360+zonekrish@users.noreply.github.com>
2022-12-25 11:11:57 -0600

    Update source code

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-# Main dependencies
 import tweepy
 import lyricsgenius
-
-# Dependencies to run every hour
-import schedule
-import time
 import os
 from server import keep_alive
 
@@ -24,41 +19,37 @@ auth = tweepy.OAuth1UserHandler(
 )
 api = tweepy.API(auth)
 
-genius = lyricsgenius.API(os.environ['GENIUS_TOKEN'])
+genius = lyricsgenius.API(os.environ['GENIUS_TOKEN']) 
 
-def reply():
-  lyrics = api.user_timeline(user_id=1264110032875999234, count=1)
-      
-  for tweet in lyrics:
-      lyric = tweet.text.strip()
-      tweetID = tweet.id
-  
-  song = genius.search_songs(lyric)['hits'][0]['result']
-  id = song['id']
-  songData = genius.song(id)
-  
-  songName = genius.search_songs(lyric)['hits'][0]['result']['title']
-  
-  if(songData['song']['album'] == None):
-      albumName = songName
-  else:
-      albumName = songData['song']['album']['name'].strip()
-  
-  if(albumName == 'FO3*'):
-      albumName = songName
-  
-  if(albumName.endswith('.')):
-      reply = f'"{songName}" from album {albumName}'
-  else:
-      reply = f'"{songName}" from album {albumName}.'
-  
-  client.create_tweet(text=reply, in_reply_to_tweet_id=tweetID)
-  print(reply)
+class listener(tweepy.StreamingClient):
+    def on_tweet(self, tweet):
+        lyric = tweet.text.strip()
+        tweetID = tweet.id
+
+        song = genius.search_songs(lyric)['hits'][0]['result']
+        id = song['id']
+        songData = genius.song(id)
+
+        songName = genius.search_songs(lyric)['hits'][0]['result']['title']
+
+        if(songData['song']['album'] == None):
+            albumName = songName
+        else:
+            albumName = songData['song']['album']['name'].strip()
+
+        if(albumName == 'FO3*'):
+            albumName = songName
+
+        if(albumName.endswith('.')):
+            reply = f'"{songName}" from album {albumName}'
+        else:
+            reply = f'"{songName}" from album {albumName}.'
+
+        client.create_tweet(text=reply, in_reply_to_tweet_id=tweetID)
+        print(reply)
 
 keep_alive()
 
-schedule.every().hour.at(':16').do(reply)
-
-while True:
-  schedule.run_pending()
-  time.sleep(1)
+streamingClient = listener(os.environ['BEARER_TOKEN'])
+streamingClient.add_rules(tweepy.StreamRule('from:frankolyricsbot'))
+streamingClient.filter()
