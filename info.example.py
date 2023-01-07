3e8e16058b92fb107918a1a6e525c974016156f2
zonekrish <107716360+zonekrish@users.noreply.github.com>
2023-01-07 11:56:56 -0600

    Add example file

---
+++ info.example.py
@@ -0,0 +1,22 @@
+# Enter your Twitter API and Genius API credentials here
+# Rename this to info.py so it's hidden by gitignore
+# These are fake values, they won't work
+
+credentials = {
+    'API_KEY': 'jfsdakfklsafjkdsajklf;da;lfldsak',
+    'API_SECRET': 'kds;hjfdsjlafdkslajfkdsa;kflas',
+    'BEARER_TOKEN': r'jkdsljkfldsajlk;fdsadjisa',
+    'ACCESS_TOKEN': 'jdsakjfkslajf;lakfdslja',
+    'ACCESS_SECRET': 'kdfsjkflsajk;dfsa;jldsaffd',
+    'GENIUS_TOKEN': 'nkfdsjakf;sajkfajk;lsad'
+}
+
+# Enter the account username you want yoru Twitter bot to reply to
+# Optionally, you can add a noAlbum list for songs that have funky album names on Genius
+# If you have no idea what that means, you can leave it blank 
+# 'noAlbum': []
+
+info = {
+    'account': 'frankolyricsbot',
+    'noAlbum': ['FO3*', 'Blonded Los Santos 97.8 FM [GTA V]', '7" Vinyl Single']
+}
