2d4b6be5f08c558a65e78fb8b8d2595f0d651008
zonekrish <107716360+zonekrish@users.noreply.github.com>
2023-02-18 08:56:47 -0600

    Put code under main method

--- reply.py
+++ reply.py
@@ -28,52 +28,56 @@ genius = Genius(credentials['GENIUS_TOKEN'])
 # Set up no-album list
 noAlbum = info['noAlbum']
 
-# Get tweets from target account
-tweets = api.user_timeline(screen_name=info['target'], count=1)
-myTweets = api.user_timeline(screen_name=info['account'], count=1)
+def main():
+    # Get tweets from target account
+    tweets = api.user_timeline(screen_name=info['target'], count=1)
+    myTweets = api.user_timeline(screen_name=info['account'], count=1)
 
-# Get tweet content
-for tweet in tweets:
-    lyric = tweet.text.strip()
-    tweetId = tweet.id
+    # Get tweet content
+    for tweet in tweets:
+        lyric = tweet.text.strip()
+        tweetId = tweet.id
 
-for tweet in myTweets:
-    inReplyTo = tweet.in_reply_to_status_id
+    for tweet in myTweets:
+        inReplyTo = tweet.in_reply_to_status_id
 
-# Main lines of code that searches for the lyric
-song = genius.search_lyrics(lyric)
-id = song['sections'][0]['hits'][0]['result']['id']
-songData = genius.song(id)
+    # Main lines of code that searches for the lyric
+    song = genius.search_lyrics(lyric)
+    id = song['sections'][0]['hits'][0]['result']['id']
+    songData = genius.song(id)
 
-songName = song['sections'][0]['hits'][0]['result']['title']
+    songName = song['sections'][0]['hits'][0]['result']['title']
 
-# Get song name, album name
-if songData['song']['album'] == None:
-    albumName = songName
-else:
-    albumName = songData['song']['album']['name'].strip()
-
-for i in range(len(noAlbum)):
-    if noAlbum[i] in albumName:
+    # Get song name, album name
+    if songData['song']['album'] == None:
         albumName = songName
+    else:
+        albumName = songData['song']['album']['name'].strip()
 
-if albumName.endswith('.'):
-    reply = f'"{songName}" from album {albumName}'
-else:
-    reply = f'"{songName}" from album {albumName}.'
+    for i in range(len(noAlbum)):
+        if noAlbum[i] in albumName:
+            albumName = songName
 
-url = song['sections'][0]['hits'][0]['result']['song_art_image_url']
+    if albumName.endswith('.'):
+        reply = f'"{songName}" from album {albumName}'
+    else:
+        reply = f'"{songName}" from album {albumName}.'
 
-# Print + tweet reply
-print(reply)
-if (inReplyTo == tweetId):
-    print('I already replied!')
-else:
-    client.create_tweet(text=reply, in_reply_to_tweet_id=tweetId)
+    url = song['sections'][0]['hits'][0]['result']['song_art_image_url']
 
-    # Get album cover file
-    getImage(url)
+    # Print + tweet reply
+    print(reply)
+    if (inReplyTo == tweetId):
+        print('I already replied!')
+    else:
+        client.create_tweet(text=reply, in_reply_to_tweet_id=tweetId)
 
-    #Update profile image
-    api.update_profile_image('temp.jpg')
-    os.remove('temp.jpg')
+        # Get album cover file
+        getImage(url)
+
+        #Update profile image
+        api.update_profile_image('temp.jpg')
+        os.remove('temp.jpg')
+
+if __name__ == '__main__':
+    main()
