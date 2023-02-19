59dfeabdc5ed0e53314ad3d0587bd8d7f15adb43
zonekrish <107716360+zonekrish@users.noreply.github.com>
2023-02-19 11:23:02 -0600

    Tweak some stuff

--- reply.py
+++ reply.py
@@ -12,7 +12,7 @@ def main():
         credentials['ACCESS_TOKEN'],
         credentials['ACCESS_SECRET']
     )
-
+    
     auth = tweepy.OAuth1UserHandler(
         credentials['API_KEY'],
         credentials['API_SECRET'],
@@ -20,49 +20,47 @@ def main():
         credentials['ACCESS_SECRET']
     )
     api = tweepy.API(auth)
-
+    
     # Log into Genius
     genius = Genius(credentials['GENIUS_TOKEN'])
-
+    
     # Set up no-album list
     noAlbum = info['noAlbum']
-
+    
     # Get tweets from target account
     tweets = api.user_timeline(screen_name=info['target'], count=1)
     myTweets = api.user_timeline(screen_name=info['account'], count=1)
-
+    
     # Get tweet content
     for tweet in tweets:
         lyric = tweet.text.strip()
         tweetId = tweet.id
-
+    
     for tweet in myTweets:
         inReplyTo = tweet.in_reply_to_status_id
-
+    
     # Main lines of code that searches for the lyric
     song = genius.search_songs(lyric)
     id = song['hits'][0]['result']['id']
     songData = genius.song(id)
-
+    
     songName = song['hits'][0]['result']['title']
-
+    
     # Get song name, album name
     if songData['song']['album'] == None:
         albumName = songName
     else:
         albumName = songData['song']['album']['name'].strip()
-
+    
     for i in range(len(noAlbum)):
         if noAlbum[i] in albumName:
             albumName = songName
-
+    
     if albumName.endswith('.'):
         reply = f'"{songName}" from album {albumName}'
     else:
         reply = f'"{songName}" from album {albumName}.'
-
-    url = song['hits'][0]['result']['song_art_image_url']
-
+    
     # Print + tweet reply
     print(reply)
     
